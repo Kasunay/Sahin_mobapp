7210434c5fd23928f21c2e271880834ebbd21228
Kasunay <giraysahinn@hotmail.com>
2024-12-15 19:23:39 +0100

    Fragments sample

--- app/src/main/java/com/example/sahin_mobapp/MainActivity.kt
+++ app/src/main/java/com/example/sahin_mobapp/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.sahin_mobapp
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var showingFragmentA = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, FragmentA())
+                .commit()
+        }
+
+        findViewById<Button>(R.id.buttonToggle).setOnClickListener {
+            toggleFragment()
+        }
+    }
+
+    private fun toggleFragment() {
+        val newFragment: Fragment = if (showingFragmentA) FragmentB() else FragmentA()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, newFragment)
+            .commit()
+
+        showingFragmentA = !showingFragmentA
     }
 }
